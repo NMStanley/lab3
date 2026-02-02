f2f69c2ccf52d5c4dd528db0fa41805afda8341a
NMStanley <nicholas.stanley.m05@gmail.com>
2026-02-01 21:08:47 -0500

    part 2: Add Items and Display Sorted List

--- Lab3_chipms217_add.py
+++ Lab3_chipms217_add.py
@@ -1,7 +1,7 @@
-from lab3_chipms217_list import camping_items
+from Lab3_chipms217_list import camping_items
 camping_items.append("axe")
 camping_items.append("tape")
 camping_items.append("frying pan")
 camping_items.append("mug")
 camping_items.append("utensils")
-sorted(camping_items, reversed = True)
+print(sorted(camping_items, reverse=True))
