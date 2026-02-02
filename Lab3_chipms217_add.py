2660801f100116d4c8f0e0dfeb985b548912faef
NMStanley <nicholas.stanley.m05@gmail.com>
2026-02-01 20:48:50 -0500

    Part 1 and 2 of lab 3

---
+++ Lab3_chipms217_add.py
@@ -0,0 +1,7 @@
+from lab3_chipms217_list import camping_items
+camping_items.append("axe")
+camping_items.append("tape")
+camping_items.append("frying pan")
+camping_items.append("mug")
+camping_items.append("utensils")
+sorted(camping_items, reversed = True)
---
+++ Lab3_chipms217_list.py
@@ -0,0 +1,3 @@
+camping_items = ["tent poles","tarp","camp stove","hammock","fruit","headlamp","first aid kit","propane","multitool","matches"]
+print(len(camping_items))
+print(sorted(camping_items))
